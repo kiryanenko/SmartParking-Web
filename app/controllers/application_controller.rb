7c5799a3628ce63da5c0bdf19e33cfd597daad74
Alexander <kiryanenkoav@mail.ru>
2018-03-04 20:25:28 +0300

    Url locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :parking_places
-  resources :parkings
-  get 'control_panel/main'
-  resources :sensors
-  devise_for :users
-  root 'main#index'
-  get 'main/index'
+  scope "(:locale)", locale: /ru|en/ do
+    resources :parking_places
+    resources :parkings
+    get 'control_panel/main'
+    resources :sensors
+    devise_for :users
+    root 'main#index'
+    get 'main/index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
