5ced44ee67c4a572ef6146d0307cd14b5bacbbe6
Alexander <kiryanenkoav@mail.ru>
2018-03-08 22:35:06 +0300

    Add classes for maps

--- app/assets/javascripts/parking_places.js
+++ app/assets/javascripts/parking_places.js
@@ -1,31 +1,33 @@
 //= require map
 
-let map;
-let parkingPlaceMarker;
+'use strict';
 
-function initParkingPlaceMap(location, area, editable = false) {
-    if (location == null) {
-        location = parkingAreaCenter(area);
+
+class ParkingPlaceMap {
+    constructor(map, location, area, editable = false) {
+        if (location == null) {
+            location = parkingAreaCenter(area);
+        }
+
+        this.map = new google.maps.Map(map, {
+            zoom: 17,
+            center: location
+        });
+
+        addParkingArea(this.map, area);
+
+
+        this.parkingPlaceMarker = new google.maps.Marker({
+            position: location,
+            map: this.map,
+            draggable: editable
+        });
     }
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 17,
-        center: location
-    });
+    setCoordField() {
+        let field = document.getElementById('parking_place_coord');
+        let pos = this.parkingPlaceMarker.position;
 
-    addParkingArea(map, area);
-
-
-    parkingPlaceMarker = new google.maps.Marker({
-        position: location,
-        map: map,
-        draggable: editable
-    });
-}
-
-function setCoordField() {
-    let field = document.getElementById('parking_place_coord');
-    let pos = parkingPlaceMarker.position;
-
-    field.value = 'POINT (' + pos.lat() + ' ' + pos.lng() + ')';
+        field.value = 'POINT (' + pos.lat() + ' ' + pos.lng() + ')';
+    }
 }
--- app/assets/javascripts/parkings.js
+++ app/assets/javascripts/parkings.js
@@ -1,39 +1,43 @@
 //= require map
 
-let map;
-let area_polygon;
+'use strict';
 
-function initParkingAreaMap(area, editable = false) {
-    if (area.length === 0 && editable) {
-        area = [
-            MAP_CENTER,
-            {lat: MAP_CENTER.lat, lng: MAP_CENTER.lng + 0.002},
-            {lat: MAP_CENTER.lat + 0.002, lng: MAP_CENTER.lng}
-        ];
+
+class ParkingAreaMap {
+    constructor(map, area, editable = false) {
+        if (area.length === 0 && editable) {
+            area = [
+                MAP_CENTER,
+                {lat: MAP_CENTER.lat, lng: MAP_CENTER.lng + 0.002},
+                {lat: MAP_CENTER.lat + 0.002, lng: MAP_CENTER.lng}
+            ];
+        }
+        let center = parkingAreaCenter(area);
+
+        this.map = new google.maps.Map(map, {
+            zoom: 17,
+            center: center
+        });
+        this.area_polygon = addParkingArea(this.map, area, editable)
     }
-    let center = parkingAreaCenter(area);
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 17,
-        center: center
-    });
-
-    area_polygon = addParkingArea(map, area, editable)
-}
-
-function removePolygonPoint() {
-    if (area_polygon.getPath().length > 3) {
-        area_polygon.getPath().removeAt(0);
+    removePolygonPoint() {
+        if (this.area_polygon.getPath().length > 3) {
+            this.area_polygon.getPath().removeAt(0);
+        }
     }
-}
 
-function setAreaField() {
-    let field = document.getElementById('parking_area');
-    let area = area_polygon.getPath().getArray();
-    area.push(area[0]);
+    setAreaField() {
+        let field = document.getElementById('parking_area');
+        let area = this.area_polygon.getPath().getArray();
+        area.push(area[0]);
 
-    let res = 'POLYGON ((';
-    res += area.map((coord) => { console.log(coord); return coord.lat() + ' ' + coord.lng() }).join(', ');
-    res += '))';
-    field.value = res;
+        let res = 'POLYGON ((';
+        res += area.map((coord) => {
+            console.log(coord);
+            return coord.lat() + ' ' + coord.lng()
+        }).join(', ');
+        res += '))';
+        field.value = res;
+    }
 }
