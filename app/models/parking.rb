da1e3292026496d122c7f5054f7e7b9ae79328ec
Alexander <kiryanenkoav@mail.ru>
2018-03-09 15:15:43 +0300

    Add scope :find_for_user

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -4,6 +4,8 @@ class Parking < ApplicationRecord
   belongs_to :user
   has_many :parking_places
 
+  scope :find_for_user, ->(id, user) { find_by! id: id, user: user }
+
   before_validation :ensure_times_both_nil
   validates :title, :area, presence: true
 
@@ -11,14 +13,14 @@ class Parking < ApplicationRecord
     where(user: user).order(:id)
   end
 
-  def self.find_for_user(id, user)
-    find_by!(id: id, user: user)
-  end
-
   def ensure_times_both_nil
     if start_time.nil? or end_time.nil?
       self.start_time = nil
       self.end_time = nil
     end
+  end
+
+  def self.unset_changed
+
   end
 end
