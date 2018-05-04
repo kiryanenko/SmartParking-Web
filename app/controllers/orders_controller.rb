882f5cb41d561e65a2f3cc6d2549d5a4219724ba
Alexander <kiryanenkoav@mail.ru>
2018-05-04 10:59:49 +0300

    user_orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.user_oders current_user
+    @orders = Order.user_orders current_user
   end
 
   # GET /orders/1
@@ -16,10 +16,6 @@ class OrdersController < ApplicationController
   # GET /orders/new
   def new
     @order = Order.new
-  end
-
-  # GET /orders/1/edit
-  def edit
   end
 
   # POST /orders
@@ -35,30 +31,6 @@ class OrdersController < ApplicationController
         format.html { render :new }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :parking_place
+
+  scope :find_for_user, ->(id, user) { find_by! id: id, user: user }
+  scope :user_orders, ->(user) { where(user: user).order(:id) }
 end
