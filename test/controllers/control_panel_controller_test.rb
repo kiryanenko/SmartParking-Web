e024d63e36127c2756ea79cd2b8559e06e10da38
Alexander <kiryanenkoav@mail.ru>
2018-05-08 23:03:26 +0300

    Add tests for control panel controller

--- test/controllers/control_panel_controller_test.rb
+++ test/controllers/control_panel_controller_test.rb
@@ -5,12 +5,31 @@ class ControlPanelControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     @user = users(:one)
-    sign_in @user
   end
 
-  # Отображение главной страницы контрольной панели
+  # Отобразить главную страницу контрольной панели для авторизованного пользователя
   test "should get main" do
+    sign_in @user
     get control_panel_main_url
     assert_response :success
+  end
+
+  # В экшене main перенаправить на страницу авторизации для не авторизованного пользователя
+  test "in main should redirect to auth" do
+    get control_panel_main_url
+    assert_redirected_to new_user_session_path
+  end
+
+  # Отобразить страницу заказов для авторизованного пользователя
+  test "should get orders" do
+    sign_in @user
+    get control_panel_orders_url
+    assert_response :success
+  end
+
+  # В экшене main перенаправить на страницу авторизации для не авторизованного пользователя
+  test "in orders should redirect to auth" do
+    get control_panel_orders_url
+    assert_redirected_to new_user_session_path
   end
 end
