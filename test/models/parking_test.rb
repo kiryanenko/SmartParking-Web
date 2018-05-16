c0f7d453b504f255520dc9f771cbee8e51ae0572
Alexander <kiryanenkoav@mail.ru>
2018-05-16 19:54:32 +0300

    Add tests on parking model

--- test/models/parking_test.rb
+++ test/models/parking_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class ParkingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Стоянка присутствует в заданном положении
+  test "parking at location" do
+    parking = parkings(:one)
+    coord = {lat: parking.area.coordinates[0][0].first - 1, lng: parking.area.coordinates[0][0].second}
+    radius = 1
+    assert Parking.parkings_at_location(coord, radius).find(parking.id)
+  end
+
+  # Стоянка не присутствует в заданном положении
+  test "parking not at location" do
+    parking = parkings(:one)
+    coord = {lat: parking.area.coordinates[0][0].first - 2, lng: parking.area.coordinates[0][0].second}
+    radius = 1
+    assert_raise ActiveRecord::RecordNotFound do
+      Parking.parkings_at_location(coord, radius).find(parking.id)
+    end
+  end
+
+  # Стоянка в заданном положении была отфильтрована
+  test "parking at location filtered by params" do
+    parking = parkings(:can_not_book)
+    coord = {lat: parking.area.coordinates[0][0].first - 1, lng: parking.area.coordinates[0][0].second}
+    radius = 2
+    params = {can_book: true}
+    assert_raise ActiveRecord::RecordNotFound do
+      ParkingPlace.parking_places_at_location(coord, radius, params).find(parking.id)
+    end
+  end
 end
