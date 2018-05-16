549e6f5ffd0ed81cbdd8f331a23163d047762d08
Alexander <kiryanenkoav@mail.ru>
2018-05-16 19:36:43 +0300

    Add test the state identical last do not create

--- test/models/parking_state_test.rb
+++ test/models/parking_state_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
+# При добавлении состояния идентичного последнему, запись не добавляется
 class ParkingStateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the state identical last do not create" do
+    last_state = parking_states(:last_state)
+    assert_no_difference 'ParkingState.count' do
+      ParkingState.set_state(last_state.parking_place, last_state.free)
+    end
+  end
 end
