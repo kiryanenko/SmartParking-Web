f37b67d0670c7b6161b0f0bf60fa200683f78613
Alexander <kiryanenkoav@mail.ru>
2018-05-19 18:42:49 +0300

    Fix set state

--- app/models/parking_state.rb
+++ app/models/parking_state.rb
@@ -2,18 +2,17 @@ class ParkingState < ApplicationRecord
   belongs_to :parking_place
 
   def self.set_state(parking_place, free)
-    changed_state = false
-    last_status = where(parking_place: parking_place).last
-    if last_status.nil? || last_status.free != free
+    if parking_place.free != free
       create parking_place: parking_place, free: free
-      changed_state = true
+      parking_place.changed_state = true
     else
+      last_status = where(parking_place: parking_place).last
       last_status.update updated_at: Time.now
     end
 
     parking_place.free = free
     parking_place.connected = true
-    parking_place.changed_state ||= changed_state
+    parking_place.changed_state = true  # FIXME
     parking_place.save
   end
 end
